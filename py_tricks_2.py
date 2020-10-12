e48bf1735c05669bf5a0f9f75c20fa877251564f
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-10-12 20:09:31 +0400

    Update py_tricks_2.py

--- py_tricks_2.py
+++ py_tricks_2.py
@@ -8,3 +8,9 @@
 def is_leap(year):
     """The shortest, the easiest way to detect leap year"""
     return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+
+
+# Never mix tabs and spaces
+# A line break between functions
+# Two line breaks between classes
+# Add a space after ','
