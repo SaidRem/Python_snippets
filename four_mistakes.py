48063c9e83ec7d6be9a1c4a3e1a5005a2b9d26a9
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-10-30 20:40:06 +0400

    Add files via upload

---
+++ four_mistakes.py
@@ -0,0 +1,87 @@
+# mistake #1
+# Do not use mutable in const
+
+class House:
+    cats = []
+
+    def __init__(self):
+        pass
+
+
+my_house = House()
+office = House()
+my_house.cats.append('Tom')
+print(my_house.cats)
+# >> ['Tom']
+print(office.cats)
+# >> ['Tom']   # Tom in office!!! -> Do not use mutable  in const
+
+
+# How to make it right
+
+class House:
+    cats: list = None   # use None for const
+
+    def __init__(self):
+        self.cats = []
+
+
+my_house = House()
+office = House()
+my_house.cats.append('Tom')
+print(my_house.cats)
+# >>> ['Tom']
+print(office.cats)
+# >>> []
+
+
+# Mistake #2
+# Using defaults in functions
+import time as tm
+from datetime import datetime
+
+
+def create_log_entry(user, action, time=datetime.now()):
+    """This function will return the same time once the scripts run."""
+    return f'{time}: {user} {action}'
+
+
+print(create_log_entry('Sam', 'smoking'))
+tm.sleep(3)
+print(create_log_entry('Sam', 'walks away'))
+
+
+# How to make it right.
+# Move the function in body of the function.
+
+
+def create_log_entry(user, action, time=None):
+    time = datetime.now()
+    return f'{time}: {user} {action}'
+
+
+print(create_log_entry('Sam', 'smoking'))
+tm.sleep(3)
+print(create_log_entry('Sam', 'walks away'))
+
+
+# Mistake #3
+# Using int and bool in dict.
+# To use True and False, bools and ints make them str.
+Vacab = {
+    'True': 'It is true',
+    'False': 'It is false',
+    '0': 'No',
+    '1': 'Yes'
+}
+
+
+# Mistake #4
+# Usig set for fast calc somtime unappropriate
+
+numbers = [i for i in range(10)]
+
+# use in for fast search in list
+
+if 5 in numbers:
+    print('Five is here')
