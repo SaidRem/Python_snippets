ed43717f0d427b8df654f36d7b282ac40ea6016c
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-11-03 22:20:06 +0400

    Update logger_decorator.py

--- logger_decorator.py
+++ logger_decorator.py
@@ -9,6 +9,7 @@ def my_logger(orig_func):
     
     @wraps(orig_func)
     def wrapper(*args, **kwargs):
+        """Here might be some computations"""
         result = orig_func(*args, **kwargs)
         logging.info(f'The func {orig_func.__name__} run with\n'
                      f'- args: {args}\n'
